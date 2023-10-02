a7984ea90107964af8f416c73dcc02c20177cf0a
nikolas <nikpost@gmx.com>
2023-10-02 15:42:31 +0200

    add ftrack

--- ini.py
+++ ini.py
@@ -1,7 +1,7 @@
 import sys
 import numpy
 import os
-
+import ftrack_api as ft
 for key in os.environ.keys():
     print(key +"=" + os.environ[key])
-    
+ses = ft.Session(server_url='https://krab.ftrackapp.com',api_key='YWJhNjkzMGItMDU2Zi00MzQ3LTk0M2EtNjQwYWFkYjcxYTMwOjo1ZWU1NzYwZi01NjkxLTQ2YmUtYThjOS0wODY4ZGM0MjA0YjI',api_user='robox@tut.by')
