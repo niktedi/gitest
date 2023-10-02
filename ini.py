d42084836ad0f19647895a27d0dd9e478c733a2c
nikolas <nikpost@gmx.com>
2023-10-02 16:50:22 +0200

    deleted example

--- ini.py
+++ ini.py
@@ -6,7 +6,5 @@ for key in os.environ.keys():
     pass
     #print(key +"=" + os.environ[key])
 ses = ft.Session(server_url='https://krab.ftrackapp.com',api_key='YWJhNjkzMGItMDU2Zi00MzQ3LTk0M2EtNjQwYWFkYjcxYTMwOjo1ZWU1NzYwZi01NjkxLTQ2YmUtYThjOS0wODY4ZGM0MjA0YjI',api_user='robox@tut.by')
-user = ses.query('User').first()
-with ses.auto_populating(False):  # For demonstration purpose only.
-    print(user.items())
+
 asset=ses.query('Asset where name is "nasos"')
