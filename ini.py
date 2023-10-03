c0f894a52c1b16b1e433ed673af235f743a40bb7
nikolas <nikpost@gmx.com>
2023-10-03 11:28:09 +0200

    new file added

--- ini.py
+++ ini.py
@@ -7,4 +7,5 @@ for key in os.environ.keys():
     #print(key +"=" + os.environ[key])
 ses = ft.Session(server_url='https://krab.ftrackapp.com',api_key='YWJhNjkzMGItMDU2Zi00MzQ3LTk0M2EtNjQwYWFkYjcxYTMwOjo1ZWU1NzYwZi01NjkxLTQ2YmUtYThjOS0wODY4ZGM0MjA0YjI',api_user='robox@tut.by')
 
-asset=ses.query('Asset where name is "nasos"')
+asset=ses.query('Asset where name is "nasos"').first()
+print(asset['versions'][0]['components'][2]['component_locations'][0]['location']['id'])
